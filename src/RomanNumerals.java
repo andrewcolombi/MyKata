bbaaf724a6eeacb71962621aebaa54ff35440ce4
Andrew Colombi <andrew.colombi@gmail.com>
2016-05-14 08:48:28 -0400

    All single letters and numbers 1-5

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -11,14 +11,37 @@
 public class RomanNumerals {
 	public String ArabicToRoman (int number) {
 		if (number >= 1) {
-			if (number == 1) {
-				return "I";
+			if (number < 5) {
+				if (number == 1) {
+					return "I";
+				}
+				else if (number == 2) {
+					return "II";
+				}
+				else if (number == 3) {
+					return "III";
+				}
+				else if (number == 4) {
+					return "IV";
+				}
 			}
-			else if (number == 2) {
-				return "II";
+			else if (number == 5) {
+				return "V";
 			}
-			else if (number == 3) {
-				return "III";
+			else if (number == 10) {
+				return "X";
+			}
+			else if (number == 50) {
+				return "L";
+			}
+			else if (number == 100) {
+				return "C";
+			}
+			else if (number == 500) {
+				return "D";
+			}
+			else if (number == 1000) {
+				return "M";
 			}
 			
 			return "Error";
--- src/Test.java
+++ src/Test.java
@@ -15,7 +15,38 @@ public class Test {
 	
 	@org.junit.Test
 	public void WhenPassed3ItReturnsTheRomanNumeral() {		
-		assertEquals("II", number.ArabicToRoman(3));
+		assertEquals("III", number.ArabicToRoman(3));
+	}
+	
+	@org.junit.Test
+	public void WhenPassed4ItReturnsTheRomanNumeral() {		
+		assertEquals("IV", number.ArabicToRoman(4));
+	}
+	
+	
+	@org.junit.Test
+	public void WhenPassed_10_ItReturnsTheRomanNumeral() {		
+		assertEquals("X", number.ArabicToRoman(10));
+	}
+	
+	@org.junit.Test
+	public void WhenPassed_50_ItReturnsTheRomanNumeral() {		
+		assertEquals("L", number.ArabicToRoman(50));
+	}
+	
+	@org.junit.Test
+	public void WhenPassed_100_ItReturnsTheRomanNumeral() {		
+		assertEquals("C", number.ArabicToRoman(100));
+	}
+	
+	@org.junit.Test
+	public void WhenPassed_500_ItReturnsTheRomanNumeral() {		
+		assertEquals("D", number.ArabicToRoman(500));
+	}
+	
+	@org.junit.Test
+	public void WhenPassed_1000_ItReturnsTheRomanNumeral() {		
+		assertEquals("M", number.ArabicToRoman(1000));
 	}
 	
 	@org.junit.Test
